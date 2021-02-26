72025b6e5a632ce28e4d4719d0374fdd2050e9c4
kamilszelag86 <kamilsz_86@o2.pl>
2021-02-26 17:35:41 +0100

    CurrentUser

---
+++ src/main/java/pl/coderslab/whereismystuff/user/service/CurrentUser.java
@@ -0,0 +1,22 @@
+package pl.coderslab.whereismystuff.user.service;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collection;
+
+public class CurrentUser extends User {
+
+    private final pl.coderslab.whereismystuff.user.entity.User user;
+
+    public CurrentUser(String username, String password,
+                       Collection<? extends GrantedAuthority> authorities,
+                       pl.coderslab.whereismystuff.user.entity.User user) {
+        super(username, password, authorities);
+        this.user = user;
+    }
+
+    public pl.coderslab.whereismystuff.user.entity.User getUser() {
+        return user;
+    }
+}
--- src/main/java/pl/coderslab/whereismystuff/web/HomeController.java
+++ src/main/java/pl/coderslab/whereismystuff/web/HomeController.java
@@ -1,7 +1,11 @@
 package pl.coderslab.whereismystuff.web;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import pl.coderslab.whereismystuff.user.entity.User;
+import pl.coderslab.whereismystuff.user.service.CurrentUser;
 
 @Controller
 public class HomeController {
@@ -14,6 +18,13 @@ public class HomeController {
     @GetMapping("/about")
     public String about() {
         return "about";
+    }
+
+    @GetMapping("/admin")
+    @ResponseBody
+    public String admin(@AuthenticationPrincipal CurrentUser currentUser) {
+        User entityUser = currentUser.getUser();
+        return "Hello " + entityUser.getUsername();
     }
 
 }
